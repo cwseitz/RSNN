73f9e01c59fb24e12519081c4c1e9ad66d71e401
Clayton Seitz <cwseitz2@gmail.com>
2021-05-22 00:08:26 -0400

    strip and port to python 3

---
+++ hebb/__init__.py
@@ -0,0 +1,4 @@
+from .connectivity import *
+from .learning_rule import *
+from .network_dynamics import *
+from .transfer_function import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup = setup(
+    name='hebb',
+    description='A package for Hebbian learning',
+    version='2.0',
+    packages=find_packages(),
+)
