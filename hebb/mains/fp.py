86eb00f3280757b946d6d864b0b7644cbc743a21
cwseitz <cwseitz2@gmail.com>
2021-09-02 19:55:48 -0500

    update core fns

--- hebb/mains/fp.py
+++ hebb/mains/fp.py
@@ -4,44 +4,16 @@ from matplotlib import cm
 from hebb.util import *
 from hebb.models import *
 
-n_in = 100
-n_rec = 100
-p_e = 0.8
+n_in = 20
+n_rec = 60
+p_e = 1.0
 p_xx = np.array([[0.2,0.2],
                  [0.2,0.2]])
 nsteps = 100
-units = 100
 dt = 1
 
-net = ExInLIF(n_in, n_rec, p_xx, p_e=p_e)
-net.in_cmg.run_generator()
-net.rec_cmg.run_generator()
-plt.imshow(net.rec_cmg.weights)
+spikes = poisson_input(nsteps, dt, units=n_in, rates=None)
+net = ExInLIF(n_in, n_rec, p_xx, p_e=p_e, period=nsteps)
+v = net.call(spikes)
+plt.imshow(v[:,0,:], cmap='gray')
 plt.show()
-
-
-#rates = np.zeros((units, nsteps))
-#rates[np.random.randint(0, units, size=10), :] = 0.1
-
-# colors = cm.coolwarm(np.linspace(0, 1, nsteps))
-# fig, ax = plt.subplots(1,3)
-#
-# nsims = 1000
-# spike_tensor = []
-# for i in range(nsims):
-#     spikes = poisson_input(nsteps, dt, units=units, rates=None)
-#     spike_tensor.append(spikes)
-# spike_tensor = np.array(spike_tensor)
-# input = np.sum(spike_tensor, axis=1)
-#
-# ax[0].imshow(spike_tensor[0], cmap='gray')
-#
-# for i in range(input.shape[-1]):
-#     ax[1].plot(input[i])
-#
-# for i in range(input.shape[-1]):
-#     vals, bins = np.histogram(input[:, i])
-#     ax[2].plot(bins[:-1], vals, color=colors[i])
-#
-# plt.tight_layout()
-# plt.show()
