eaed1329d7d78cf88c956ed7c6a5c10ce897cd01
cwseitz <cwseitz2@gmail.com>
2021-09-28 21:24:46 -0500

    update

--- hebb/mains/lif_tf.py
+++ hebb/mains/lif_tf.py
@@ -18,20 +18,30 @@ N = 1 #number of neurons
 dt = 0.001 #1ms
 T =  0.1 #100ms
 tau_ref = 0.003 #3ms
-dI = 0.0004
-currents = np.arange(-0.04,0.04,dI)
+
+#Input current
+currents = np.linspace(-0.04,0.04,100)
+
 sc = np.zeros_like(currents)
 sigma = 0.02
-iters = 20
-rates = np.zeros((iters,len(currents)))
+iters = 10
+taus = np.linspace(dt, 100*dt, 100)
+rates = np.zeros((iters,len(taus),len(currents)))
+
 
 for i in range(iters):
-    for j, current in enumerate(currents):
-        input = current*np.ones((N, batches, int(round(T/dt))+1))
-        input += sigma*np.random.normal(0,1,size=input.shape)
-        lif = LIF(T, dt, tau_ref, N=N, tau=100*dt, input=input, batches=batches)
-        lif.call()
-        rates[i,j] = np.sum(lif.Z[0,0,:])/T
+    for j, tau in enumerate(taus):
+        for k, current in enumerate(currents):
+            input = current*np.ones((N, batches, int(round(T/dt))+1))
+            input += sigma*np.random.normal(0,1,size=input.shape)
+            lif = LIF(T, dt, tau_ref, N=N, tau=tau, input=input, batches=batches)
+            lif.call()
+            rates[i,j,k] = np.sum(lif.Z[0,0,:])/T
 
-plt.scatter(currents, np.mean(rates, axis=0), color='black', marker='x')
+plt.imshow(np.mean(rates, axis=0), cmap='coolwarm')
+plt.xlabel('I')
+plt.ylabel('$\tau$')
 plt.show()
+
+# plt.scatter(currents, np.mean(rates, axis=0), color='black', marker='x')
+# plt.show()
