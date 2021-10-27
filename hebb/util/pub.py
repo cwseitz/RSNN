c524df78110e0d0a46a78dc20f767ad7ee2b7bae
cwseitz <cwseitz2@gmail.com>
2021-10-27 17:31:34 -0400

    update pubs

--- hebb/util/pub.py
+++ hebb/util/pub.py
@@ -8,6 +8,80 @@ from hebb.util import *
 from hebb.models import *
 from matplotlib.ticker import FormatStrFormatter
 from .format_ax import *
+mpl.rcParams['text.usetex'] = True
+
+def fig_0():
+
+    x = np.linspace(-5, 5, 100)
+    x1, x2 = 0, 0
+    sigma1, sigma2 = 1, 1
+    y1 = np.exp(-(0.5*(x+x1)**2)/sigma1**2)
+    y2 = np.exp(-(0.5*(x+x2)**2)/sigma2**2)
+    y3 = 0.5*np.ones_like(x)
+
+    fig, ax = plt.subplots(2,2, figsize=(6,5))
+    ax[0,0].plot(x, y1, color='blue', label=r'$\Gamma_{ij}$')
+    ax[0,0].plot(x, y2, color='red', linestyle='--', label=r'$\Gamma_{ji}$')
+    #ax[0,0].plot(x, y3, color='cyan', label=r'$\Gamma_{0}$')
+    ax[0,0].set_xlim([0,4])
+    ax[0,0].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    ax[0,0].text(-0.1, 1.075, 'A', transform=ax[0,0].transAxes, size=16, weight='bold')
+    format_ax(ax[0,0], ax_is_box=False)
+    ax[0,0].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    ax[0,0].legend(loc='upper right')
+
+    p12 = y1*(1-y2)*(1-y3)
+    p21 = y2*(1-y1)*(1-y3)
+    p0 = y3*(1-y1)*(1-y2)
+    z = p12 + p21 + p0
+    p12 /= z
+    p21 /= z
+    p0 /= z
+
+    ax[0,1].plot(x, p12, color='blue', label=r'$p_{ij}$')
+    ax[0,1].plot(x, p21, color='red', linestyle='--', label=r'$p_{ji}$')
+    ax[0,1].plot(x, p0, color='cyan', label=r'$p_{0}$')
+    ax[0,1].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    ax[0,1].set_xlim([0,4])
+    format_ax(ax[0,1], ax_is_box=False)
+    ax[0,1].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    ax[0,1].legend(loc='upper right')
+
+    x = np.linspace(-5, 5, 100)
+    x1, x2 = 0, 0
+    sigma1, sigma2 = 2, 1
+    y1 = np.exp(-(0.5*(x+x1)**2)/sigma1**2)
+    y2 = np.exp(-(0.5*(x+x2)**2)/sigma2**2)
+    y3 = 0.5*np.ones_like(x)
+
+
+    ax[1,0].plot(x, y1, color='blue', label=r'$\Gamma_{ij}$')
+    ax[1,0].plot(x, y2, color='red', linestyle='--', label=r'$\Gamma_{ji}$')
+    #ax[1,0].plot(x, y3, color='cyan', label=r'$\Gamma_{0}$')
+    ax[1,0].set_xlim([0,4])
+    ax[1,0].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    ax[1,0].text(-0.1, 1.075, 'B', transform=ax[1,0].transAxes, size=16, weight='bold')
+    format_ax(ax[1,0], ax_is_box=False)
+    ax[1,0].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    ax[1,0].legend(loc='upper right')
+
+    p12 = y1*(1-y2)*(1-y3)
+    p21 = y2*(1-y1)*(1-y3)
+    p0 = y3*(1-y1)*(1-y2)
+    z = p12 + p21 + p0
+    p12 /= z
+    p21 /= z
+    p0 /= z
+
+    ax[1,1].plot(x, p12, color='blue', label=r'$p_{ij}$')
+    ax[1,1].plot(x, p21, color='red', linestyle='--', label=r'$p_{ji}$')
+    ax[1,1].plot(x, p0, color='cyan', label=r'$p_{0}$')
+    ax[1,1].set_xlim([0,4])
+    format_ax(ax[1,1], ax_is_box=False)
+    ax[1,1].legend(loc='upper right')
+    ax[1,1].set_xlabel(r'$|\Delta \mathbf{r}_{ij}|$', fontsize=12)
+    plt.tight_layout()
+    plt.show()
 
 def fig_1(N,sigma,q):
 
@@ -177,6 +251,7 @@ def fig_2():
     p_vec = gauss_net_shared(N, unique, sigma, q) #numerical solution
     ax[0].plot(unique, avgs_arr/N, color='red')
     ax[0].plot(unique, p_vec/N, color='blue')
+    ax[0].text(-0.1, 1.075, 'A', transform=ax[0].transAxes, size=14, weight='bold')
     format_ax(ax[0],
               xlabel=r'$\Delta r_{ij}$',
               label_fontsize='large',
@@ -190,6 +265,7 @@ def fig_2():
     p_vec = gauss_net_shared(N, unique, sigma, q) #numerical solution
     ax[1].plot(unique, avgs_arr/N, color='red')
     ax[1].plot(unique, p_vec/N, color='blue')
+    ax[1].text(-0.1, 1.075, 'B', transform=ax[1].transAxes, size=14, weight='bold')
     format_ax(ax[1],
               xlabel=r'$\Delta r_{ij}$',
               label_fontsize='large',
@@ -201,6 +277,7 @@ def fig_2():
     net = GaussianNetwork(N, sigma*np.ones((M,M)), q)
     unique, avgs_arr = gauss_net_shared_exp(net) #experimental solution
     p_vec = gauss_net_shared(N, unique, sigma, q) #numerical solution
+    ax[2].text(-0.1, 1.075, 'C', transform=ax[2].transAxes, size=14, weight='bold')
     ax[2].plot(unique, avgs_arr/N, color='red')
     ax[2].plot(unique, p_vec/N, color='blue')
     format_ax(ax[2],
@@ -249,7 +326,7 @@ def fig_3(N=100, q1=0.2, q2=0.8, sigma_e=5, sigma_i=5, p_e=0.8):
     ax10 = fig.add_subplot(gs[5, 3:4])
 
     ax0.set_axis_off()
-    ax0.text(0.1, 0.8, 'A', transform=ax0.transAxes, size=14, weight='bold')
+    #ax0.text(0.1, 0.8, 'A', transform=ax0.transAxes, size=14, weight='bold')
 
     net = ExInGaussianNetwork(N, sigma_e, sigma_i, q1, p_e=p_e)
     custom_lines = [Line2D([0],[0],color='red', lw=4),Line2D([0],[0],color='dodgerblue', lw=4)]
@@ -257,7 +334,7 @@ def fig_3(N=100, q1=0.2, q2=0.8, sigma_e=5, sigma_i=5, p_e=0.8):
     ax1.set_title(f'$q={q1}$')
     ax1.legend(custom_lines, ['E', 'I'], loc='upper left')
     ax1.set_axis_off()
-    ax1.text(-0.1, 1.0, 'B', transform=ax1.transAxes, size=14, weight='bold')
+    ax1.text(-0.1, 1.0, 'A', transform=ax1.transAxes, size=14, weight='bold')
 
     """
     Fix q, p_e, bias_e, and bias_i and generate maps of sigma_e vs sigma_i
@@ -286,7 +363,7 @@ def fig_3(N=100, q1=0.2, q2=0.8, sigma_e=5, sigma_i=5, p_e=0.8):
               xlabel=r'$\sigma_{I}$',
               ylabel=r'$\langle I_{I}\rangle/N_{I}$',
               ax_is_box=False)
-    ax2.text(-0.4, 1.1, 'C', transform=ax2.transAxes, size=14, weight='bold')
+    ax2.text(-0.4, 1.1, 'B', transform=ax2.transAxes, size=14, weight='bold')
 
     ax3.imshow(n_ei_in, origin='lower', cmap='coolwarm')
     format_ax(ax3,
@@ -325,7 +402,7 @@ def fig_3(N=100, q1=0.2, q2=0.8, sigma_e=5, sigma_i=5, p_e=0.8):
     ax6.set_title(f'$q={q2}$')
     ax6.legend(custom_lines, ['E', 'I'], loc='upper left')
     ax6.set_axis_off()
-    ax6.text(-0.1, 1.0, 'D', transform=ax6.transAxes, size=14, weight='bold')
+    ax6.text(-0.1, 1.0, 'C', transform=ax6.transAxes, size=14, weight='bold')
 
     """
     Fix q, p_e, bias_e, and bias_i and generate maps of sigma_e vs sigma_i
@@ -349,7 +426,7 @@ def fig_3(N=100, q1=0.2, q2=0.8, sigma_e=5, sigma_i=5, p_e=0.8):
     n_ie_in /= N*p_e
 
     ax7.plot(np.mean(n_ii_in,axis=0), color='red')
-    ax7.text(-0.3, 1.1, 'E', transform=ax7.transAxes, size=14, weight='bold')
+    ax7.text(-0.3, 1.1, 'D', transform=ax7.transAxes, size=14, weight='bold')
     format_ax(ax7,
               xlabel=r'$\sigma_{I}$',
               ylabel=r'$\langle I_{I}\rangle/N_{I}$',
